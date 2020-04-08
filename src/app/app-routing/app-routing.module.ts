23a552d45da860361aac7d3bdd744ad8f7ec491f
AHaaland <drhaaland@gmail.com>
2020-04-08 09:59:18 -0400

    2 files modified, 1 added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AndrewCompComponent } from '../andrew-comp/andrew-comp.component';
+import { HelloComponent } from '../hello.component';
+
+const routes: Routes = [
+  {path: 'andrew', component: AndrewCompComponent},
+  {path: 'hello', component:HelloComponent, data: { title: 'Test' }},
+  {path: '', redirectTo: '/andrew', pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AndrewCompComponent } from './andrew-comp/andrew-comp.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,AndrewCompComponent],
