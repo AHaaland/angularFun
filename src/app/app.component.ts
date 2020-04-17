0ce22c801dc867f305facede0c9cccc991d3ba42
AHaaland <drhaaland@gmail.com>
2020-04-17 11:15:33 -0400

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { AndrewCompComponent } from './andrew-comp/andrew-comp.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+@NgModule ({ imports:[
+   MatToolbarModule,MatButtonModule],
+   exports:[MatToolbarModule,MatButtonModule]
+  })
+  
 export class AppComponent  {
   name = 'Angular';
 }
